bff1cf1af36c3e8a096139d1f7dab1e3542f215a
Adrien Jouve <adrien.jouve@adn-dev.fr>
2024-05-18 08:29:59 +0200

    add traces to push pull button

--- actionner/PushPullButton.cpp
+++ actionner/PushPullButton.cpp
@@ -1,6 +1,17 @@
 #include <Arduino.h>
 #include "PushPullButton.h"
 
+#define DEBUG
+
+#ifdef DEBUG
+#define DEBUG_MSG_ONELINE(x) Serial.print(F(x))
+#define DEBUG_MSG(x) Serial.println(F(x))
+#define DEBUG_MSG_VAR(x) Serial.println(x)
+#else
+#define DEBUG_MSG(x) // define empty, so macro does nothing
+#define DEBUG_MSG_VAR(x)
+#define DEBUG_MSG_ONELINE(x)
+#endif
 
 PushPullButton::PushPullButton(int pin, unsigned long upTime) :
   DigitalOutput(pin),
@@ -10,6 +21,7 @@ PushPullButton::PushPullButton(int pin, unsigned long upTime) :
 void PushPullButton::Enable() {
   DigitalOutput::Enable();
   mStartTime = millis();
+  DEBUG_MSG("PushPullButton::Enable");
 }
 
 void PushPullButton::Handle() {
@@ -17,5 +29,6 @@ void PushPullButton::Handle() {
     && ((millis() - mStartTime) > mUpTime)) {
       DigitalOutput::Disable();
       mStartTime = 0;
+      DEBUG_MSG("PushPullButton::Disable");
   }
 }
